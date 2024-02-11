32eddb7579155fcb228dbf0c8d3aad5741bcf3a9
Iryna Lelyk <irunochka771@gmail.com>
2024-02-11 16:13:35 +0200

    fix build

--- auction-client/src/pages/HomePage.tsx
+++ auction-client/src/pages/HomePage.tsx
@@ -6,15 +6,6 @@ import { FundsPack } from "src/components/FundsPack";
 export const HomePage: React.FC = () => {
   return (
     <>
-<<<<<<< HEAD
-      {/* посилання щоб перейти*/}
-      <Link to="/search">Search Results Page</Link>
-      <br />
-      <Link to="/lots/:lotId">Lot Page</Link>
-      <br />
-      <Link to="/add-lot">Add Lot</Link>
-=======
->>>>>>> main
       <Hero />
       <LotsPack title="Активні лоти" />
       <LotsPack title="Популярні лоти" />
