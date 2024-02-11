7c33adf1cf852a3fc035d70ef61f95731ea0ba94
Olaxander <mittsykh@ukr.net>
2024-02-11 16:32:25 +0200

    create register

--- auction-api/api/source/configuration/setupRoutes.ts
+++ auction-api/api/source/configuration/setupRoutes.ts
@@ -2,9 +2,11 @@ import { Container } from "inversify";
 import { Express } from "express";
 import authenticateToken from "@module/middlewares/authorizationMiddleware";
 import { getAuthorizationRoutes } from "./routes/authorizationRoutes";
+import { getAccountsRoutes } from "./routes/accountsRoutes";
 
 
 export function setupRoutes(app: Express, container: Container) {
     //app.use('/accounts', authenticateToken,  getAccountMetaGraphControllerRouter(container));
     app.use('/user', getAuthorizationRoutes(container))
+    app.use('/accounts', getAccountsRoutes(container))
 }
--- auction-api/api/source/tests/mockLotRepository.ts
+++ auction-api/api/source/tests/mockLotRepository.ts
@@ -1,33 +1,34 @@
-import { Bid, IRepository, Range } from "core";
+import { Lot, IRepository, Range } from "core";
 import { injectable } from "inversify";
 import { FilterQuery, QueryOptions } from "mongoose";
 
 @injectable()
-export class BidRepository implements IRepository<Bid> {
-    
-
-    getAllAsync(): Promise<Bid[]> {
+export class LotRepository implements IRepository<Lot> {
+    getAllAsync(): Promise<Lot[]> {
         throw new Error("Method not implemented.");
     }
     getAllCount(): Promise<number> {
         throw new Error("Method not implemented.");
     }
-    getByRangeAsync(range: Range): Promise<Bid[]> {
+    getByRangeAsync(range: Range): Promise<Lot[]> {
         throw new Error("Method not implemented.");
     }
-    getByIdAsync(id: string, populatePath?: string | undefined): Promise<Bid | null> {
+    getByIdAsync(id: string, populatePath?: string | undefined): Promise<Lot | null> {
         throw new Error("Method not implemented.");
     }
-    createAsync(entity: Bid): Promise<Bid> {
+    createAsync(entity: Lot): Promise<Lot> {
         throw new Error("Method not implemented.");
     }
-    filterAsyncByRange(filter: FilterQuery<any>, range: Range, sortOptions?: QueryOptions<any> | undefined, populatePath?: string | undefined): Promise<Bid[] | null> {
+    filterAsyncByRange(filter: FilterQuery<any>, range: Range, sortOptions?: QueryOptions<any> | undefined, populatePath?: string | undefined): Promise<Lot[] | null> {
         throw new Error("Method not implemented.");
     }
-    updateAsync(id: string, entity: Bid): Promise<any> {
+    updateAsync(id: string, entity: Lot): Promise<any> {
         throw new Error("Method not implemented.");
     }
     deleteAsync(id: string): Promise<any> {
         throw new Error("Method not implemented.");
     }
+    
+
+  
 }
